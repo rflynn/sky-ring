4c538e90409a1f4436971952fb7af21b99a7c27e
pizza <parseerror@gmail.com>
2010-04-15 14:17:27 -0400

    refactored simpler code, cleaned stuff up. better example

--- sky-ring.py
+++ sky-ring.py
@@ -14,16 +14,18 @@
 # edit: looks like Bellshill, Scotland. The 56th parallel, so that makes sense.
 
 # Model/Assumptions:
-#  - left is due East, right is due West, horizon is 0° geometric, top is 90° geometric (i.e. straight up)
-#  - center of image at the horizon is due South
+#  - center of image at the horizon is directly perpendicular to the Eqautor (due South from the Northern hemisphere)
+#  - from the Northern Hemisphere the left is due East, right is due West, horizon is 0° geometric, top is 90° geometric (i.e. straight up)
 
 # TODO's
 #  - size of sun is arbitrary, figure out how to use ephem's sun.size (and maybe sun.magnitude)
 #  - breaks in the sunlight (clouds) are psuedo-random to simulate real climate data. find some real climate data!
 
-import datetime, sys, random, re
+import ephem # astrological calculations: Sun's position
+import cairo # graphics
+import datetime, sys, re
+from random import random,randint,choice
 from math import pi,radians,sin,cos,sqrt
-import ephem, cairo
 
 height = 500.0
 width = 600.0
@@ -67,45 +69,67 @@ when = datetime.datetime(now.year, 6, 21, 0, 0, 0)
 o.date = when
 sunrise = when
 
+# FIXME: fake clouds
+# day-to-day profiles of cloudiness used to feed random
+cloud_profile = (
+	#sun on, sun off
+	(720,  10), # sunny
+	(300,  30),
+	(180,  30),
+	(120,  40),
+	( 60,  30), 
+	( 30,  40),
+	(  5,   5), # back and forth
+	(  1, 720), # cloudy
+)
+
+alpha_mul = 1.0 # ongoing alpha multiplier
+sun_radius = height / 400
+
+# loop-invariant calculations (python should do this on its own but doesn't)
+twopi = 2 * pi
+halfpi = pi / 2
+halfwidth = width / 2
+
+# NOTE: about half of this code pertains to generating psuedo-random cloud patterns and solar intensity
+# stuff we wouldn't have to do if we had real climate data
 while when.month < 12 or when.day <= 21: # ...through the winter solstice
 	# we have sunrise, now calculate sunset
 	o.date = when
 	s = o.next_setting(sun).tuple()
 	sunset = datetime.datetime(s[0],s[1],s[2],s[3],s[4],int(s[5]))
 	print('sunrise=%s sunset=%s' % (sunrise,sunset)) # show progress
-	# FIXME: fake overcast days
-	if random.random() > 0.8:
-		when += datetime.timedelta(days=1)
+	# FIXME: fake clouds
+	cl = choice(cloud_profile) # choose a cloudiness profile each day
+	cloudnext = when + (step * randint(0,cl[0]))
+	cloudlen = randint(1,cl[1])
+	alpha_today = random() * 0.5 # gives a nice "streakiness"
+	while when < sunset:
+		if when == cloudnext:
+			when += step * cloudlen
+			cloudnext = when + (step * randint(0,cl[0]))
+			cloudlen = 60 + randint(1,cl[1])
 		o.date = when
-	else:
-		# FIXME: fake clouds
-		cloudnext = when + (step * (random.randint(0,200)))
-		cloudlen = random.randint(1,100)
-		while when < sunset:
-			if when == cloudnext:
-				when += (step * cloudlen)
-				cloudnext = when + (step * (random.randint(0,180)))
-				cloudlen = 60 + random.randint(1,60)
-			o.date = when
-			sun = ephem.Sun(o)
-			# translate sun position
-			x = width - (float(sun.az) / (2*pi) * width)
-			# horizon = 0°, top = 90° (pi/2 radians)
-			y0 = (float(sun.alt) / (pi/2))
-			y = (height - horizon) - (y0 * (height - horizon))
-			"""
-			print('date=%s az=%f alt=%f x=%f y=%f mag=%s size=%s' % \
-				(o.date, float(sun.az), float(sun.alt), x, y, sun.mag, sun.size))
-			"""
-			# TODO: set intensity of sun based on weather and position
-			# c2f6eb
-			# FIXME: force lower intensity on the edges
-			cr.set_source_rgba(0.76, 0.964, 0.921, 0.3+(random.random()*0.2)-((abs(width/2-x))/width))
-			# draw
-			cr.arc(x, y, 2.0-((abs(width/2-x))/width), 0, 2 * pi) # FIXME: tweak sun size, middle=biggest
-			cr.fill()
-			# loop
-			when += step
+		sun = ephem.Sun(o)
+		# translate sun position
+		x = width - float(sun.az) / twopi * width
+		# horizon = 0°, top = 90° (pi/2 radians)
+		y0 = float(sun.alt) / halfpi
+		y = (height - horizon) - (y0 * (height - horizon))
+		# TODO: set intensity of sun based on weather and position
+		alpha = alpha_today
+		alpha -= abs(halfwidth - x) / width # FIXME: force lower intensity on the edges
+		alpha *= alpha_mul
+		# rgb(0xc2f6eb)
+		cr.set_source_rgba(0.76, 0.964, 0.921, alpha)
+		# draw
+		radius = sun_radius
+		radius -= abs(halfwidth - x) / width # FIXME: tweak sun size, middle=biggest
+		cr.arc(x, y, radius, 0, twopi) 
+		cr.fill()
+		# loop
+		when += step
+	alpha_mul *= 0.997 # reduce alpha the lower in the sky we go
 	# calculate next sunrise
 	r = o.next_rising(sun).tuple()
 	sunrise = datetime.datetime(r[0],r[1],r[2],r[3],r[4],int(r[5]))
